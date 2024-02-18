3bbca5ead70ebf780fac536ec59ce6fa82d7bd5c
Andreas Baumgart <a.baumgart@haw-hamburg.de>
2024-02-18 19:13:51 +0100

    step e

--- flipper.py
+++ flipper.py
@@ -8,3 +8,7 @@ print(data.sheet_names)
 print(data.sheet_names[0])
 print("new co0nten")
 # text some text and ModuleNot
+systemData = data.parse(sheet_name=sheetname[0],skiprows=1)
+print(systemData)
+print(systemData["parameter"][0])
+if 
