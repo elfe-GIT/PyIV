d902d18de39db63ea37930b8372b12060890e6fd
Andreas Baumgart <a.baumgart@haw-hamburg.de>
2024-02-18 16:57:35 +0100

    Start

---
+++ flipper.py
@@ -0,0 +1,9 @@
+print("Hello World")
+import os
+import pandas as pd
+file_name = "parameter.xlsx"
+sheetname = ["system","seeds"]
+data = pd.ExcelFile(file_name)
+print(data.sheet_names)
+print(data.sheet_names[0])
+print("new co0nten")
